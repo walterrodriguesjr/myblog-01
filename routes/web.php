ea99f212c4d31205c6ea660c5da56d4606e9d60a
Walter <walterrjr.86@gmail.com>
2022-06-29 17:20:46 -0300

    aula-12-ok-feito alterações na view welcome, apagado controllers, routes e views desnecessários

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('dashboard', function () {
-    return view('layouts.dashboard');
-});
-
 Auth::routes([
     'register' => false
 ]);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::group(['prefix' => 'dashboard', 'middleware' => ['auth']], function () {
+    
+Route::group(['prefix' => 'dashboard', 'middleware' => ['web', 'auth']], function () {
     Route::get('/', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard.index');
 });
